e4f62de8a68622f7b058fe9872fe56342aea4181
Alicia Kay <alicia.kaye1@gmail.com>
2017-06-08 19:03:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,5 @@ $(document).ready(function() {
       $("#accordion", "#work").accordion({
       	active: false
     	});
-      $("#work").accordion({
-      	heightStyle: "content"
-                	});
-      $("#work").accordion({
-      	collapsible: true
-                	});
-      $("#work").accordion({
-      	active: false
-    	});
+
   });
